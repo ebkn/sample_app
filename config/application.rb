aba7cd8677ba0216bfc4e0ac1372378969a7a7b3
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-07-12 18:18:02 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ module SampleApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,22 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
-  resources :users
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
-
   resources :password_resets, only: [:new, :create, :edit, :update]
-
   resources :microposts, only: [:create, :destroy]
-
-  root 'static_pages#home'
+  resources :relationships, only: [:create, :destroy]
 end
